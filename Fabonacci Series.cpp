9b414c119be95886e9fcd6882458730d8974d016
B-Chaudhry <59957683+B-Chaudhry@users.noreply.github.com>
2020-02-24 15:12:12 +0500

    Add files via upload

---
+++ Fabonacci Series.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main()
+{
+	int i, n, a=0, b=1, fab=0;
+	cout<< "enter a number n ";
+cin>> n;
+	if (n > 1);
+		for(i= 1, i<n; i++;)
+		cout<< n;
+		fab = a+b;
+		a=b;
+		b= fab;
+		return 0;
+}
